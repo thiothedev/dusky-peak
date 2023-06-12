353c51628ce07a0248b562b6bb89009bb70b42db
thiothedev <thiothedev@proton.me>
2023-06-12 08:29:45 +0200

    Creating the Game class

---
+++ src/Classes/Game.cpp
@@ -0,0 +1,157 @@
+#include "../Headers/Game.hpp"
+
+using namespace dp;
+
+// Vertices and Indices
+GLfloat vertices[] = {
+  -0.5f,  -0.5f, 0.f, 1.f, 0.f, 0.f,
+   0.0f,  -0.5f, 0.f, 0.f, 1.f, 0.f,
+   0.5f,  -0.5f, 0.f, 0.f, 0.f, 1.f,
+  -0.25f,  0.0f, 0.f, 1.f, 1.f, 0.f,
+   0.25f,  0.0f, 0.f, 0.f, 1.f, 1.f,
+   0.0f,   0.5f, 0.f, 1.f, 0.f, 1.f,
+};
+GLuint indices[] = {
+  0, 1, 3,
+  1, 2, 4,
+  3, 4, 5,
+};
+
+// Callbacks
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+// Constructor and Destructor
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+  this->defaultShader->Delete();
+  this->VAO1->Delete();
+  this->VBO1->Delete();
+  this->EBO1->Delete();
+  glfwTerminate();
+
+  delete this->defaultShader;
+  delete this->VAO1;
+  delete this->VBO1;
+  delete this->EBO1;
+}
+
+// Functions
+
+bool Game::Initialize()
+{
+  this->initializeGlfw();
+  if (!this->initializeWindow()) return false;
+  if (!this->initializeGlew()) return false;
+  if (!this->initializeMembers()) return false;
+  return true;
+}
+
+void Game::Run()
+{
+  // Main Loop
+  while (!glfwWindowShouldClose(this->window))
+  {
+    this->update();
+    this->render();
+  }
+}
+
+// Initializers
+
+void Game::initializeGlfw()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+bool Game::initializeWindow()
+{
+  this->window = glfwCreateWindow(
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    WINDOW_TITLE,
+    NULL,
+    NULL
+  );
+  if (this->window == NULL)
+  {
+    std::cerr << "Failed to initialize a GLFW window!" << std::endl;
+    glfwTerminate();
+    return false;
+  }
+  glfwMakeContextCurrent(this->window);
+
+  // Clear Color
+  GLclampf red   = 0.0f;
+  GLclampf green = 0.3f;
+  GLclampf blue  = 0.3f;
+  GLclampf alpha = 1.0f;
+  glClearColor(red, green, blue, alpha);
+
+  // Callbacks
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  return true;
+}
+
+bool Game::initializeGlew()
+{
+  GLenum err = glewInit();
+  if (err != GLEW_OK)
+  {
+    std::cerr << "Failed to initialize GLEW!" << std::endl;
+    std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+    glfwTerminate();
+    return false;
+  }
+  std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
+  return true;
+}
+
+bool Game::initializeMembers()
+{
+  this->defaultShader = new Shader("src/Shaders/default.vert", "src/Shaders/default.frag");
+  this->VAO1 = new VAO();
+  this->VBO1 = new VBO(vertices, sizeof(vertices));
+  this->EBO1 = new EBO(indices, sizeof(indices));
+
+  this->VAO1->Bind();
+  this->VBO1->Bind();
+  this->EBO1->Bind();
+
+  this->VAO1->LinkAttrib(*this->VBO1, 0, 3, GL_FLOAT, 6 * sizeof(GLfloat), (void*)0);
+  this->VAO1->LinkAttrib(*this->VBO1, 1, 3, GL_FLOAT, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+
+  this->VAO1->Unbind();
+  this->VBO1->Unbind();
+  this->EBO1->Unbind();
+
+  return true;
+}
+
+// Private Functions
+
+void Game::update()
+{
+  glfwPollEvents();
+}
+
+void Game::render()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  this->defaultShader->Use();
+  VAO1->Bind();
+  glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, NULL);
+  glfwSwapBuffers(this->window);
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,11 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#include "Headers/Shader.hpp"
-#include "Headers/VAO.hpp"
-#include "Headers/VBO.hpp"
-#include "Headers/EBO.hpp"
-#include "Utils/File.hpp"
+#include "Headers/Game.hpp"
 
 // Constants
 const unsigned int WINDOW_WIDTH  = 800;
@@ -14,98 +10,11 @@ const unsigned int WINDOW_HEIGHT = 600;
 const char*        WINDOW_TITLE  = "GLFW";
 const unsigned int INFO_LOG_SIZE = 512;
 
-// Vertices and Indices
-GLfloat vertices[] = {
-  -0.5f,  -0.5f, 0.f, 1.f, 0.f, 0.f,
-   0.0f,  -0.5f, 0.f, 0.f, 1.f, 0.f,
-   0.5f,  -0.5f, 0.f, 0.f, 0.f, 1.f,
-  -0.25f,  0.0f, 0.f, 1.f, 1.f, 0.f,
-   0.25f,  0.0f, 0.f, 0.f, 1.f, 1.f,
-   0.0f,   0.5f, 0.f, 1.f, 0.f, 1.f,
-};
-GLuint indices[] = {
-  0, 1, 3,
-  1, 2, 4,
-  3, 4, 5,
-};
-
 int main()
 {
-  // Initializing GLFW
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  dp::Game game;
+  if (!game.Initialize()) return 1;
+  game.Run();
 
-  // Creating the Window
-  GLFWwindow* window = glfwCreateWindow(
-    WINDOW_WIDTH,
-    WINDOW_HEIGHT,
-    WINDOW_TITLE,
-    NULL,
-    NULL
-  );
-  if (window == NULL)
-  {
-    std::cerr << "Failed to initialize a GLFW window!" << std::endl;
-    glfwTerminate();
-    return 1;
-  }
-  glfwMakeContextCurrent(window);
-
-  // Initializing GLEW
-  GLenum err = glewInit();
-  if (err != GLEW_OK)
-  {
-    std::cerr << "Failed to initialize GLEW!" << std::endl;
-    std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
-    glfwTerminate();
-    return 1;
-  }
-  std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
-
-  // Clear Color
-  GLclampf red   = 0.0f;
-  GLclampf green = 0.3f;
-  GLclampf blue  = 0.3f;
-  GLclampf alpha = 1.0f;
-  glClearColor(red, green, blue, alpha);
-
-  // Shader Program
-  dp::Shader defaultShader("src/Shaders/default.vert", "src/Shaders/default.frag");
-
-  // VBO
-  dp::VAO VAO1;
-  dp::VBO VBO1(vertices, sizeof(vertices));
-  dp::EBO EBO1(indices, sizeof(indices));
-
-  VAO1.Bind();
-  VBO1.Bind();
-  EBO1.Bind();
-
-  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, sizeof(GLfloat) * 6, (void*)0);
-  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, sizeof(GLfloat) * 6, (void*)(3 * sizeof(GLfloat)));
-
-  VAO1.Unbind();
-  VBO1.Unbind();
-  EBO1.Unbind();
-
-  // Main Loop
-  while (!glfwWindowShouldClose(window))
-  {
-    glfwPollEvents();
-    glClear(GL_COLOR_BUFFER_BIT);
-    defaultShader.Use();
-    VAO1.Bind();
-    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, NULL);
-    glfwSwapBuffers(window);
-  }
-
-  // Terminating the Program
-  defaultShader.Delete();
-  VAO1.Delete();
-  VBO1.Delete();
-  EBO1.Delete();
-  glfwTerminate();
   return 0;
 }
